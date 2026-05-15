7fa6ff92b5b060efdaa88112e1fdb87b4c127c0e
Quinlzbeth <shonibare109@gmail.com@byupathway.edu>
2026-05-15 04:24:22 +0100

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,43 @@
+// Program.cs
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create first job
+        Job job1 = new Job();
+
+        // Set member variables
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        
+        // Create second job
+        Job job2 = new Job();
+
+        // Set member variables
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume instance
+        Resume myResume = new Resume();
+
+        // Set the person's name
+        myResume._name = "Elizabeth Shonibare";
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
+
     }
 }
+
